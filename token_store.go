ed7311523086dc8647cd503ef4da046afaf4d124
Mihajlo Vranic <mihajlo@strong.network>
2024-01-05 09:51:56 +0000

    Format ui data field names

--- token_store.go
+++ token_store.go
@@ -680,8 +680,8 @@ type tokenData struct {
 }
 
 type UIData struct {
-	Device     string    `json:"Device" bson:"device,omitempty"`
-	DeviceOS   string    `json:"osType" bson:"os_type,omitempty"`
+	Device     string    `json:"device_name" bson:"device,omitempty"`
+	DeviceOS   string    `json:"device_os" bson:"os_type,omitempty"`
 	CreatedAt  time.Time `bson:"created_at,omitempty"`
 	LastUsedAt time.Time `bson:"last_used_at,omitempty"`
 }
